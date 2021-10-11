1acfa80e2fe82006a20db19525c24ccbf3aa253b
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-10-11 07:29:36 +0700

    Arrow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_X01AD.mk
+    $(LOCAL_DIR)/arrow_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_X01AD-eng \
-    dot_X01AD-user \
-    dot_X01AD-userdebug      
+    arrow_X01AD-eng \
+    arrow_X01AD-user \
+    arrow_X01AD-userdebug      
--- arrow_X01AD.mk
+++ arrow_X01AD.mk
@@ -12,14 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from onclite device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
-# Inherit some common dotOS stuff
-$(call inherit-product, vendor/dot/config/common.mk)
-
-#Boot animation
-TARGET_BOOT_ANIMATION_RES := 720
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := dot_X01AD
+PRODUCT_NAME := arrow_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_BRAND := Asus
 PRODUCT_MODEL := ASUS-X01AD
@@ -31,6 +28,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.005 7349499 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
-
-# Pixel Charger
-TARGET_INCLUDE_PIXEL_CHARGER := true
